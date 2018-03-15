d3156c5a87d02371bc832211588426823cd17756
Максим <33746405+MakartsevMaksym@users.noreply.github.com>
2018-03-15 13:46:50 +0200

    Update 1-20game.py

--- 1-20game.py
+++ 1-20game.py
@@ -4,7 +4,7 @@ to_be_guessed = random.randint(1,20)
 guess = 0
 while guess != to_be_guessed:
     try:
-        guess = int(input("Введи номер: "))
+        guess = int(input("Введи номер(1-20, 0 - сдаться): "))
         if guess > 0:
             if guess > to_be_guessed:
                 print("Слишком много!")
