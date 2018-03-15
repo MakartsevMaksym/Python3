985918c6b656127621fdb4916f88040c1586217f
Максим <33746405+MakartsevMaksym@users.noreply.github.com>
2018-03-15 14:58:13 +0200

    3

--- 1-20game.py
+++ 1-20game.py
@@ -1,19 +1,24 @@
 import random
 
-to_be_guessed = random.randint(1,20)
-guess = 0
-while guess != to_be_guessed:
-    try:
-        guess = int(input("Введи номер(1-20, 0 - сдаться): "))
-        if guess > 0:
-            if guess > to_be_guessed:
-                print("Слишком много!")
-            elif guess < to_be_guessed:
-                print("Слишком мало!")
-        else:
-            print("Сдался.")
-            break
-    except ValueError:
-        print("Введи целое число!")
-else:
-    print("Угадал!")   
+def guess_number():
+	secretNumber = random.randint(1,20)
+	print('Угадай число от 1 до 20.')
+	acc = 0
+	while True:
+		try:
+			number = int(input('Введите число: '))
+			acc += 1
+			if number < secretNumber:
+				print('Больше!')
+			elif number > secretNumber:
+				print('Меньше!')
+			else:
+				print('\n		Победа!')
+				print('\n	Число, яке Вы ввели ' + str(number), sep='')
+				break
+		except ValueError:
+			print('Неверный ввод.')
+			continue
+	return acc
+times = guess_number()
+print('    Вы угадали число с ' + str(times) + ' попытки')
