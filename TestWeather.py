b6aeac1d98a542e862c71102b5ad82dff3e79bba
Максим <33746405+MakartsevMaksym@users.noreply.github.com>
2018-05-10 13:40:11 +0300

    Add files via upload

---
+++ TestWeather.py
@@ -0,0 +1,21 @@
+import requests
+
+print("------------------------------------------------")
+city = input("Введите город (Пример: Kyiv): ")
+appid = "a0d60f299cee3a1d0f3e7950a2649389"
+
+res = requests.get("http://api.openweathermap.org/data/2.5/weather", params = {'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+print("------------------------------------------------")
+print(data)
+print("------------------------------------------------")
+print("Погода для города: " + city)
+print("------------------------------------------------")
+print("Погодные условия:", data["weather"][0]["description"])
+print("Текущая температура:", int(data["main"]["temp"]))
+print("Минимальная температура на сегодня:", data["main"]["temp_min"])
+print("Максимальная температура на сегодня:", data["main"]["temp_max"])
+print("Давление: " + str(data["main"]["pressure"]) + " мм. ртутного столбика")
+print("Влажность: " + str(data["main"]["humidity"]) + "%")
+print("Скорость ветра: " + str(data["wind"]["speed"]) + " км/ч")
+print("------------------------------------------------")
